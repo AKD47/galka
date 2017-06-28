5f996d90f433615c10172750611c0b5fa5a92da4
Oleg <yastreb92@inbox.ru>
2017-06-29 01:34:09 +0300

    new

--- app/js/script.js
+++ app/js/script.js
@@ -24,4 +24,22 @@ $(document).ready(function () {
     }*/
     /*close header mobile menu*/
 
+    /*prod slider*/
+    $('.slider-for').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     arrows: false,
+     fade: true,
+     asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     asNavFor: '.slider-for',
+     dots: true,
+     centerMode: true,
+     focusOnSelect: true
+    });
+
+    /*prod slider*/
 });
